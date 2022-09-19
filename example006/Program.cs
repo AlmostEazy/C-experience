986e481883b8f04b6e94889abc4f0ebbc614d724
AlmostEazy <Antosha101992@rambler.ru>
2022-09-19 17:39:40 +0300

    feat: add example006 file

---
+++ example006/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "anton") {
+    Console.WriteLine("Yo, ANTON!");
+} else {
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
